96eccf5d080c3038b4be6cf3140d4cc18fc858a4
Emmanuel <emakouamou@yahoo.fr>
2019-03-06 11:32:31 +0100

    test commit

--- app/src/main/java/com/example/balafonchat/MainActivity.java
+++ app/src/main/java/com/example/balafonchat/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //1 er changement
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
